0589409c50ba00d208332ce83ee26cc2a509229c
will <willeneum@gmail.com>
2013-11-21 23:02:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HockeySite::Application.config.secret_key_base = 'd0df170d360baec519af328f0b2f1aa598dd441ac8877ea428ebfc143f10bf4eb33346667440bc8aff95dc0d25737b9c19d49b76a6a5702317aeafa9cb60a7d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HockeySite::Application.config.session_store :cookie_store, key: '_hockey_site_session'
